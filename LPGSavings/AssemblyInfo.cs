534138465d79168faacbea0f66a0ac7ae46be85d
Kacper Sobek <kacper.sobek@t2t-system.com>
2021-08-16 17:14:53 +0200

    Added font awesome

--- LPGSavings/AssemblyInfo.cs
+++ LPGSavings/AssemblyInfo.cs
@@ -1,3 +1,15 @@
+using System.Resources;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
+
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: Preserve(AllMembers = true)]
+[assembly: NeutralResourcesLanguage("pl-PL")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "fab")]
+[assembly: ExportFont("fa-duotone-900.ttf", Alias = "fad")]
+[assembly: ExportFont("fa-light-300.ttf", Alias = "fal")]
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "far")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "fas")]
