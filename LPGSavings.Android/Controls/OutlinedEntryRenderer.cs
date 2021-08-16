80de7ed20838a3bd991d8c9c980058bd3787b228
Kacper Sobek <kacper.sobek@t2t-system.com>
2021-08-16 17:15:55 +0200

    Preparing outline styled entry

---
+++ LPGSavings.Android/Controls/OutlinedEntryRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.Content;
+using LPGSavings.Controls;
+using LPGSavings.Droid.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(OutlinedEntry),typeof(OutlinedEntryRenderer))]
+namespace LPGSavings.Droid.Controls
+{
+
+    public class OutlinedEntryRenderer : EntryRenderer
+    {
+        public OutlinedEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            //Configure native control (TextBox)
+            if (Control != null)
+            {
+                Control.Background = null;
+            }
+
+            // Configure Entry properties
+            if (e.NewElement != null)
+            {
+
+            }
+        }
+    }
+}
---
+++ LPGSavings.iOS/Controls/OutlinedEntryRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LPGSavings.Controls;
+using LPGSavings.iOS.Controls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(OutlinedEntry),typeof(OutlinedEntryRenderer))]
+namespace LPGSavings.iOS.Controls
+{
+    public class OutlinedEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            //Configure Native control (UITextField)
+            if (Control != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+            }
+        }
+    }
+}
