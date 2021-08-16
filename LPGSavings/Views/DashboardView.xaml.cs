fd8987fca02f7393e222ce4f6a8b74dffbf9e9a2
Kacper Sobek <kacper.sobek@t2t-system.com>
2021-08-16 17:15:38 +0200

    Preparing views.

---
+++ LPGSavings/Views/DashboardView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace LPGSavings.Views
+{
+    public partial class DashboardView : ContentView
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
